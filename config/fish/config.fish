ad8fb755a7b85d5d05d6cc1b8123568b8590f2f2
Scott Olson <scott@solson.me>
2019-07-08 04:03:04 -0600

    fish: Update git prompt style.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -207,13 +207,25 @@ end
 # function fish_right_prompt
 # end
 
-set __fish_git_prompt_char_stateseparator ''
-set __fish_git_prompt_describe_style branch
+set __fish_git_prompt_describe_style default
 set __fish_git_prompt_showcolorhints 1
 set __fish_git_prompt_showdirtystate 1
 set __fish_git_prompt_showstashstate 1
 set __fish_git_prompt_showuntrackedfiles 1
 set __fish_git_prompt_showupstream informative
+
+set __fish_git_prompt_char_dirtystate '~'
+set __fish_git_prompt_char_invalidstate '✖'
+set __fish_git_prompt_char_stagedstate '●'
+set __fish_git_prompt_char_stashstate '⚑'
+set __fish_git_prompt_char_stateseparator ''
+set __fish_git_prompt_char_untrackedfiles '?'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
+set __fish_git_prompt_char_upstream_diverged '↕'
+
+set __fish_git_prompt_color_stashstate yellow
+set __fish_git_prompt_color_untrackedfiles red
 
 function fish_title
   echo (prompt_pwd) : $_
