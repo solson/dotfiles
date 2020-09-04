b998a8265a6de1f4699bf03d56287097e8f6932d
Scott Olson <scott@solson.me>
2020-09-04 18:55:48 +0100

    scripts: Run `deno fmt`.

--- scripts/format-duration.ts
+++ scripts/format-duration.ts
@@ -1,2 +1,2 @@
 import { format_duration } from "./lib.ts";
-console.log(format_duration(parseInt(Deno.args[0])))
+console.log(format_duration(parseInt(Deno.args[0])));
--- scripts/lib.ts
+++ scripts/lib.ts
@@ -1,34 +1,34 @@
 export function divmod(x: number, y: number): [number, number] {
-    return [Math.trunc(x / y), x % y]
+  return [Math.trunc(x / y), x % y];
 }
 
 // Inspired by https://docs.raku.org/routine/polymod.
 export function polymod(x: number, mods: number[]): number[] {
-    let more = x;
-    let remainders: number[] = [];
-    for (const mod of mods) {
-        const rem = more % mod;
-        remainders.push(rem);
-        more -= rem;
-        more /= mod;
-    }
-    remainders.push(more);
-    return remainders;
+  let more = x;
+  let remainders: number[] = [];
+  for (const mod of mods) {
+    const rem = more % mod;
+    remainders.push(rem);
+    more -= rem;
+    more /= mod;
+  }
+  remainders.push(more);
+  return remainders;
 }
 
 export function format_duration(total_ms: number): string {
-    const [ms, s, m, h, d] = polymod(total_ms, [1000, 60, 60, 24])
-    const ms_hundreds = Math.round(ms / 100)
+  const [ms, s, m, h, d] = polymod(total_ms, [1000, 60, 60, 24]);
+  const ms_hundreds = Math.round(ms / 100);
 
-    if (d > 0) {
-        return `${d}d ${h}h ${m}m ${s}s`
-    } else if (h > 0) {
-        return `${h}h ${m}m ${s}s`
-    } else if (m > 0) {
-        return `${m}m ${s}s`
-    } else if (ms_hundreds > 0) {
-        return `${s}.${ms_hundreds}s`
-    } else {
-        return `${s}s`
-    }
+  if (d > 0) {
+    return `${d}d ${h}h ${m}m ${s}s`;
+  } else if (h > 0) {
+    return `${h}h ${m}m ${s}s`;
+  } else if (m > 0) {
+    return `${m}m ${s}s`;
+  } else if (ms_hundreds > 0) {
+    return `${s}.${ms_hundreds}s`;
+  } else {
+    return `${s}s`;
+  }
 }
