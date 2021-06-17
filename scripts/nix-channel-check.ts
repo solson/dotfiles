e1a5c994104cd5892a383c13f5a2c34dce0cf172
Scott Olson <scott@solson.me>
2021-06-17 01:26:03 +0100

    add shebang to nix-channel-check script

--- scripts/nix-channel-check.ts
+++ scripts/nix-channel-check.ts
@@ -1,5 +1,13 @@
+#!/usr/bin/env -S deno run --allow-net
+
 const url = "https://channels.nixos.org/nixos-unstable/packages.json.br";
-const watchedPackages = ["babashka", "deno", "keepassxc", "neovim", "firefox"];
+const watchedPackages = [
+  "babashka",
+  "deno",
+  "firefox",
+  "keepassxc",
+  "neovim",
+];
 
 const { packages } = await fetch(url).then((r) => r.json());
 
